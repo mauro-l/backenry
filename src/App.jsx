d308b3371754c6cf5c0a5ebd681a80dd9923ba55
nelidasuarez <neessu2023@gmail>
2024-05-15 18:42:13 -0300

    feat: create carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Carousel from "./components/Carousel";
 import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar";
+import NavbarSticky from "./components/NavbarSticky";
 
 function App() {
   return (
     <>
+      <Navbar />
+      <NavbarSticky />
+      <Carousel />
       <ItemListContainer />
     </>
   );
