0e758992dd4c91b9bcc0803de295a68d9fcc7264
Mauro Laime <maurol.dev@gmail.com>
2024-05-15 12:53:13 -0300

    feat: Create ProductList page under the admin section. Also, implement the initial basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import ItemListContainer from "./components/ItemListContainer";
+//import ItemListContainer from "./components/ItemListContainer";
+import ProductList from "./pages/admin/ProductList";
+import ItemListContainer from "./pages/shop/Products/ItemListContainer";
+import Home from "./pages/shop/Home/Home";
 
 function App() {
   return (
     <>
-      <ItemListContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ItemListContainer />} />
+          <Route path="/admin/products" element={<ProductList />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/admin/helpers/productList.helper.js
@@ -0,0 +1,7 @@
+export const heads = [
+  "#",
+  "Nombre del Producto",
+  "Categoria",
+  "Stock",
+  "Acciones",
+];
